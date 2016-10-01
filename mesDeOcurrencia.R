52cbdba412ce4977eb26a171083314e910519fc6
BERNARDO MONDRAGON BROZON <bmondra1@itam.mx>
2016-09-30 21:13:47 -0500

    Algo

--- mesDeOcurrencia.R
+++ mesDeOcurrencia.R
@@ -1,4 +1,60 @@
-mp22004 <- read.csv("2004mp2.csv", 
+mp22004 <- read.csv("/mp2/2004mp2.csv", 
                        colClasses = "character", 
                        na.strings = "#N/A")
+mp22004
+
+mp22005 <- read.csv("/mp2/2005mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22005
+
+mp22006 <- read.csv("/mp2/2006mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22006
+
+mp22007 <- read.csv("/mp2/2007mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22007
+
+mp22008 <- read.csv("/mp2/2008mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22008
+
+mp22009 <- read.csv("/mp2/2009mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22009
+
+mp22010 <- read.csv("/mp2/2010mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22010
+
+mp22011 <- read.csv("/mp2/2011mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22011
+
+mp22012 <- read.csv("/mp2/2012mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22012
+
+mp22013 <- read.csv("/mp2/2013mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22013
+
+mp22014 <- read.csv("/mp2/2014mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22014
+
+mp22015 <- read.csv("/mp2/2015mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22015
 
