b0b314a0602558f92a2bc5bb2e2233d02413adb1
BERNARDO MONDRAGON BROZON <bmondra1@itam.mx>
2016-09-30 21:32:02 -0500

    Limpieza de codigo

---
+++ Mordeduras Perro Tablas.R
@@ -0,0 +1,165 @@
+mpdosmilcuatro <- read.csv("/Excels/2004mp.csv", 
+                       colClasses = "character", 
+                       na.strings = "#N/A")
+
+
+mpdosmilcinco <- read.csv("/Excels/2005mp.csv", 
+                           colClasses = "character", 
+                           na.strings = "#N/A")
+
+mpdosmilseis <- read.csv("/Excels/2006mp.csv", 
+                          colClasses = "character", 
+                          na.strings = "#N/A")
+
+mpdosmilsiete <- read.csv("/Excels/2007mp.csv", 
+                         colClasses = "character", 
+                         na.strings = "#N/A")
+
+mpdosmilocho <- read.csv("/Excels/2008mp.csv", 
+                          colClasses = "character", 
+                          na.strings = "#N/A")
+
+mpdosmilnueve <- read.csv("/Excels/2009mp.csv", 
+                         colClasses = "character", 
+                         na.strings = "#N/A")
+
+mpdosmildiez <- read.csv("/Excels/2010mp.csv", 
+                          colClasses = "character", 
+                          na.strings = "#N/A")
+
+mpdosmilonce <- read.csv("/Excels/2011mp.csv", 
+                         colClasses = "character", 
+                         na.strings = "#N/A")
+
+mpdosmildoce <- read.csv("/Excels/2012mp.csv", 
+                         colClasses = "character", 
+                         na.strings = "#N/A")
+
+mpdosmiltrece <- read.csv("/Excels/2013mp.csv", 
+                         colClasses = "character", 
+                         na.strings = "#N/A")
+
+mpdosmilcatorce <- read.csv("/Excels/2014mp.csv", 
+                          colClasses = "character", 
+                          na.strings = "#N/A")
+
+mpdosmilquince <- read.csv("/Excels/2015mp.csv", 
+                            colClasses = "character", 
+                            na.strings = "#N/A")
+
+mpdosmilcuatroGE <- read.csv("/Excels/2004mp3.csv", 
+                             colClasses = "character", 
+                             na.strings = "#N/A")
+
+mpdosmilcincoGE <- read.csv("/Excels/2005mp3.csv", 
+                            colClasses = "character", 
+                            na.strings = "#N/A")
+
+mpdosmilseisGE <- read.csv("/Excels/2006mp3.csv", 
+                           colClasses = "character", 
+                           na.strings = "#N/A")
+
+mpdosmilsieteGE <- read.csv("/Excels/2007mp3.csv", 
+                            colClasses = "character", 
+                            na.strings = "#N/A")
+
+mpdosmilochoGE <- read.csv("/Excels/2008mp3.csv", 
+                           colClasses = "character", 
+                           na.strings = "#N/A")
+
+mpdosmilnueveGE <- read.csv("/Excels/2009mp3.csv", 
+                            colClasses = "character", 
+                            na.strings = "#N/A")
+
+mpdosmildiezGE <- read.csv("/Excels/2010mp3.csv", 
+                           colClasses = "character", 
+                           na.strings = "#N/A")
+
+mpdosmilonceGE <- read.csv("/Excels/2011mp3.csv", 
+                           colClasses = "character", 
+                           na.strings = "#N/A")
+
+mpdosmildoceGE <- read.csv("/Excels/2012mp3.csv", 
+                           colClasses = "character", 
+                           na.strings = "#N/A")
+
+mpdosmiltreceGE <- read.csv("/Excels/2013mp3.csv", 
+                            colClasses = "character", 
+                            na.strings = "#N/A")
+
+mpdosmilcatorceGE <- read.csv("/Excels/2014mp3.csv", 
+                              colClasses = "character", 
+                              na.strings = "#N/A")
+
+mpdosmilquinceGE <- read.csv("/Excels/2015mp3.csv", 
+                             colClasses = "character", 
+                             na.strings = "#N/A")
+
+mp22004 <- read.csv("/Excels/2004mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22004
+
+mp22005 <- read.csv("/Excels/2005mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22005
+
+mp22006 <- read.csv("/Excels/2006mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22006
+
+mp22007 <- read.csv("/Excels/2007mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22007
+
+mp22008 <- read.csv("/Excels/2008mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22008
+
+mp22009 <- read.csv("/Excels/2009mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22009
+
+mp22010 <- read.csv("/Excels/2010mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22010
+
+mp22011 <- read.csv("/Excels/2011mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22011
+
+mp22012 <- read.csv("/Excels/2012mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22012
+
+mp22013 <- read.csv("/Excels/2013mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22013
+
+mp22014 <- read.csv("/Excels/2014mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22014
+
+mp22015 <- read.csv("/Excels/2015mp2.csv", 
+                    colClasses = "character", 
+                    na.strings = "#N/A")
+mp22015
+
+
+
+
+
+
+
+
+
