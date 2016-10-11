0f10e7aef8359ca45dcfcbac877c249df8016380
Rayan <rayan.garciafa@gmail.com>
2016-10-11 18:30:50 -0500

    Modificacion

--- CodigosLecturaYLimpiezaCorregido/MordedurasPerroCodigo.R
+++ CodigosLecturaYLimpiezaCorregido/MordedurasPerroCodigo.R
@@ -18,6 +18,45 @@ for (i in 1:length(fnm)) assign(fnm[i], read.csv(fnm[i],colClasses = "character"
                                                  na.strings = "#N/A"))
 fnmujeres <- lapply(fnm, fread, sep=",")
 
+setwd("/Users/rayangf/Desktop/MordedurasPerro-m/Data/CSV/GrupoDeEdad/General")
+geg = list.files(pattern="*.csv") #Codigo para leer los csv GrupoDeEdad caso general
+for (i in 1:length(geg)) assign(geg[i], read.csv(geg[i],colClasses = "character", 
+                                                 na.strings = "#N/A"))
+gegeneral <- lapply(geg, fread, sep=",") 
+
+setwd("/Users/rayangf/Desktop/MordedurasPerro-m/Data/CSV/GrupoDeEdad/Hombres")
+geh = list.files(pattern="*.csv") #Codigo para leer los csv GrupoDeEdad caso hombres
+for (i in 1:length(geh)) assign(geh[i], read.csv(geh[i],colClasses = "character", 
+                                                 na.strings = "#N/A"))
+gehombres <- lapply(geh, fread, sep=",") 
+
+setwd("/Users/rayangf/Desktop/MordedurasPerro-m/Data/CSV/GrupoDeEdad/Mujeres")
+gem = list.files(pattern="*.csv") #Codigo para leer los csv GrupoDeEdad caso mujeres
+for (i in 1:length(gem)) assign(gem[i], read.csv(gem[i],colClasses = "character", 
+                                                 na.strings = "#N/A"))
+gemujeres <- lapply(gem, fread, sep=",") 
+
+library(data.table)  
+setwd("/Users/rayangf/Desktop/MordedurasPerro/Data/CSV/MesDeOcurrencia/General")
+mog = list.files(pattern="*.csv") #Codigo para leer los csv MesDeOcurrencia caso general
+for (i in 1:length(mog)) assign(mog[i], read.csv(mog[i],colClasses = "character", 
+                                                 na.strings = "#N/A"))
+mogeneral <- lapply(mog, fread, sep=",") 
+
+
+setwd("/Users/rayangf/Desktop/MordedurasPerro/Data/CSV/MesDeOcurrencia/Hombres")
+moh = list.files(pattern="*.csv") #Codigo para leer los csv MesDeOcurrencia caso hombres
+for (i in 1:length(moh)) assign(moh[i], read.csv(moh[i],colClasses = "character", 
+                                                 na.strings = "#N/A"))
+mohombres <- lapply(moh, fread, sep=",") 
+
+
+setwd("/Users/rayangf/Desktop/MordedurasPerro/Data/CSV/MesDeOcurrencia/Mujeres")
+mom = list.files(pattern="*.csv") #Codigo para leer los csv MesDeOcurrencia caso mujeres
+for (i in 1:length(mom)) assign(mom[i], read.csv(mom[i],colClasses = "character", 
+                                                 na.strings = "#N/A"))
+momujeres <- lapply(mom, fread, sep=",") 
+
 for (i in 1:length(fngeneral)) {
   names(fngeneral[[i]]) <- toupper(names(fngeneral[[i]]))
 }
@@ -131,6 +170,185 @@ for (j in 1:length(fnmujeres)) {
                               SEMAR <- gsub("N.A.*|S.R*|NA.", "NA", SEMAR)
                               OTRAS <- gsub("N.A.*|S.R*|NA.", "NA", OTRAS)
                               TOTAL <- gsub("N.A.*|S.R*|NA.", "NA", TOTAL)
+                              ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
+                              ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
+                              ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
+                              ESTADO <- gsub("Guan.*o.*", "Guanajuato", ESTADO)
+                              ESTADO <- gsub("M.*n.*", "Michoacan", ESTADO)
+                              ESTADO <- gsub("M.*los.*", "Morelos", ESTADO)
+                              ESTADO <- gsub("Nuevo Le.*n.*", "Nuevo Leon", ESTADO)
+                              ESTADO <- gsub("Quer.*ro.*", "Queretaro", ESTADO)
+                              ESTADO <- gsub("Zac.*cas.*", "Zacatecas", ESTADO)
+                              ESTADO <- gsub("Quint.*oo.*", "Quintana Roo", ESTADO)
+                              ESTADO <- gsub("San Luis Pot.*.*", "San Luis Potosi", ESTADO)
+                              ESTADO <- gsub("Yuca.*n.*", "Yucatan",ESTADO)
+                            }
+  )
+}
+
+for (i in 1:length(gegeneral)) {
+  names(gegeneral[[i]]) <- toupper(names(gegeneral[[i]]))
+}
+
+for (j in 1:length(gegeneral)) {
+  gegeneral[[j]] <- within( gegeneral[[j]] , 
+                            {
+                              MAYOR.A.UNO <- gsub("[ ]", "", MAYOR.A.UNO)
+                              UNO.A.CUATRO <- gsub("[ ]", "", UNO.A.CUATRO)
+                              CINCO.A.NUEVE <- gsub("[ ]", "", CINCO.A.NUEVE)
+                              DIEZ.A.CATORCE <- gsub("[ ]", "",DIEZ.A.CATORCE)
+                              QUINCE.A.DIECINUEVE <- gsub("[ ]", "", QUINCE.A.DIECINUEVE)
+                              VEINTE.A.VEINTICUATRO <- gsub("[ ]", "", VEINTE.A.VEINTICUATRO )
+                              VEINTICINCO.A.CUARENTAYCUATRO <- gsub("[ ]", "", VEINTICINCO.A.CUARENTAYCUATRO)
+                              CUARENTAYCINCO.A.CUARENTAYNUEVE <- gsub("[ ]", "", CUARENTAYCINCO.A.CUARENTAYNUEVE)
+                              CINCUENTA.A.CINCUENTAYNUEVE <- gsub("[ ]", "", CINCUENTA.A.CINCUENTAYNUEVE)
+                              SESENTA.A.SESENTAYCUATRO <- gsub("[ ]", "", SESENTA.A.SESENTAYCUATRO)
+                              MAYOR.A.SESENTAYCINCO <- gsub("[ ]", "", MAYOR.A.SESENTAYCINCO)
+                              NO.SE.REPORTO.LA.EDAD <- gsub("[ ]", "", NO.SE.REPORTO.LA.EDAD)
+                              TOTAL <- gsub("[ ]", "", TOTAL)
+                              ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
+                              ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
+                              ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
+                              ESTADO <- gsub("Guan.*o.*", "Guanajuato", ESTADO)
+                              ESTADO <- gsub("M.*n.*", "Michoacan", ESTADO)
+                              ESTADO <- gsub("M.*los.*", "Morelos", ESTADO)
+                              ESTADO <- gsub("Nuevo Le.*n.*", "Nuevo Leon", ESTADO)
+                              ESTADO <- gsub("Quer.*ro.*", "Queretaro", ESTADO)
+                              ESTADO <- gsub("Zac.*cas.*", "Zacatecas", ESTADO)
+                              ESTADO <- gsub("Quint.*oo.*", "Quintana Roo", ESTADO)
+                              ESTADO <- gsub("San Luis Pot.*.*", "San Luis Potosi", ESTADO)
+                              ESTADO <- gsub("Yuca.*n.*", "Yucatan",ESTADO)
+                            }
+  )
+}
+
+for (i in 1:length(gehombres)) {
+  names(gehombres[[i]]) <- toupper(names(gehombres[[i]]))
+}
+
+for (j in 1:length(gehombres)) {
+  gehombres[[j]] <- within( gehombres[[j]] , 
+                            {
+                              MAYOR.A.UNO <- gsub("[ ]", "", MAYOR.A.UNO)
+                              UNO.A.CUATRO <- gsub("[ ]", "", UNO.A.CUATRO)
+                              CINCO.A.NUEVE <- gsub("[ ]", "", CINCO.A.NUEVE)
+                              DIEZ.A.CATORCE <- gsub("[ ]", "",DIEZ.A.CATORCE)
+                              QUINCE.A.DIECINUEVE <- gsub("[ ]", "", QUINCE.A.DIECINUEVE)
+                              VEINTE.A.VEINTICUATRO <- gsub("[ ]", "", VEINTE.A.VEINTICUATRO )
+                              VEINTICINCO.A.CUARENTAYCUATRO <- gsub("[ ]", "", VEINTICINCO.A.CUARENTAYCUATRO)
+                              CUARENTAYCINCO.A.CUARENTAYNUEVE <- gsub("[ ]", "", CUARENTAYCINCO.A.CUARENTAYNUEVE)
+                              CINCUENTA.A.CINCUENTAYNUEVE <- gsub("[ ]", "", CINCUENTA.A.CINCUENTAYNUEVE)
+                              SESENTA.A.SESENTAYCUATRO <- gsub("[ ]", "", SESENTA.A.SESENTAYCUATRO)
+                              MAYOR.A.SESENTAYCINCO <- gsub("[ ]", "", MAYOR.A.SESENTAYCINCO)
+                              NO.SE.REPORTO.LA.EDAD <- gsub("[ ]", "", NO.SE.REPORTO.LA.EDAD)
+                              TOTAL <- gsub("[ ]", "", TOTAL)
+                              ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
+                              ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
+                              ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
+                              ESTADO <- gsub("Guan.*o.*", "Guanajuato", ESTADO)
+                              ESTADO <- gsub("M.*n.*", "Michoacan", ESTADO)
+                              ESTADO <- gsub("M.*los.*", "Morelos", ESTADO)
+                              ESTADO <- gsub("Nuevo Le.*n.*", "Nuevo Leon", ESTADO)
+                              ESTADO <- gsub("Quer.*ro.*", "Queretaro", ESTADO)
+                              ESTADO <- gsub("Zac.*cas.*", "Zacatecas", ESTADO)
+                              ESTADO <- gsub("Quint.*oo.*", "Quintana Roo", ESTADO)
+                              ESTADO <- gsub("San Luis Pot.*.*", "San Luis Potosi", ESTADO)
+                              ESTADO <- gsub("Yuca.*n.*", "Yucatan",ESTADO)
+                            }
+  )
+}
+
+
+
+for (i in 1:length(gemujeres)) {
+  names(gemujeres[[i]]) <- toupper(names(gemujeres[[i]]))
+}
+
+for (j in 1:length(gemujeres)) {
+  gemujeres[[j]] <- within( gemujeres[[j]] , 
+                            {
+                              MAYOR.A.UNO <- gsub("[ ]", "", MAYOR.A.UNO)
+                              UNO.A.CUATRO <- gsub("[ ]", "", UNO.A.CUATRO)
+                              CINCO.A.NUEVE <- gsub("[ ]", "", CINCO.A.NUEVE)
+                              DIEZ.A.CATORCE <- gsub("[ ]", "",DIEZ.A.CATORCE)
+                              QUINCE.A.DIECINUEVE <- gsub("[ ]", "", QUINCE.A.DIECINUEVE)
+                              VEINTE.A.VEINTICUATRO <- gsub("[ ]", "", VEINTE.A.VEINTICUATRO )
+                              VEINTICINCO.A.CUARENTAYCUATRO <- gsub("[ ]", "", VEINTICINCO.A.CUARENTAYCUATRO)
+                              CUARENTAYCINCO.A.CUARENTAYNUEVE <- gsub("[ ]", "", CUARENTAYCINCO.A.CUARENTAYNUEVE)
+                              CINCUENTA.A.CINCUENTAYNUEVE <- gsub("[ ]", "", CINCUENTA.A.CINCUENTAYNUEVE)
+                              SESENTA.A.SESENTAYCUATRO <- gsub("[ ]", "", SESENTA.A.SESENTAYCUATRO)
+                              MAYOR.A.SESENTAYCINCO <- gsub("[ ]", "", MAYOR.A.SESENTAYCINCO)
+                              NO.SE.REPORTO.LA.EDAD <- gsub("[ ]", "", NO.SE.REPORTO.LA.EDAD)
+                              TOTAL <- gsub("[ ]", "", TOTAL)
+                              ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
+                              ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
+                              ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
+                              ESTADO <- gsub("Guan.*o.*", "Guanajuato", ESTADO)
+                              ESTADO <- gsub("M.*n.*", "Michoacan", ESTADO)
+                              ESTADO <- gsub("M.*los.*", "Morelos", ESTADO)
+                              ESTADO <- gsub("Nuevo Le.*n.*", "Nuevo Leon", ESTADO)
+                              ESTADO <- gsub("Quer.*ro.*", "Queretaro", ESTADO)
+                              ESTADO <- gsub("Zac.*cas.*", "Zacatecas", ESTADO)
+                              ESTADO <- gsub("Quint.*oo.*", "Quintana Roo", ESTADO)
+                              ESTADO <- gsub("San Luis Pot.*.*", "San Luis Potosi", ESTADO)
+                              ESTADO <- gsub("Yuca.*n.*", "Yucatan",ESTADO)
+                            }
+  )
+}
+
+for (i in 1:length(mogeneral)) {
+  names(mogeneral[[i]]) <- toupper(names(mogeneral[[i]]))
+}
+
+for (j in 1:length(mogeneral)) {
+  mogeneral[[j]] <- within( mogeneral[[j]] , 
+                            {
+                              ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
+                              ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
+                              ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
+                              ESTADO <- gsub("Guan.*o.*", "Guanajuato", ESTADO)
+                              ESTADO <- gsub("M.*n.*", "Michoacan", ESTADO)
+                              ESTADO <- gsub("M.*los.*", "Morelos", ESTADO)
+                              ESTADO <- gsub("Nuevo Le.*n.*", "Nuevo Leon", ESTADO)
+                              ESTADO <- gsub("Quer.*ro.*", "Queretaro", ESTADO)
+                              ESTADO <- gsub("Zac.*cas.*", "Zacatecas", ESTADO)
+                              ESTADO <- gsub("Quint.*oo.*", "Quintana Roo", ESTADO)
+                              ESTADO <- gsub("San Luis Pot.*.*", "San Luis Potosi", ESTADO)
+                              ESTADO <- gsub("Yuca.*n.*", "Yucatan",ESTADO)
+                            }
+  )
+}
+
+for (i in 1:length(mohombres)) {
+  names(mohombres[[i]]) <- toupper(names(mohombres[[i]]))
+}
+
+for (j in 1:length(mohombres)) {
+  mohombres[[j]] <- within( mohombres[[j]] , 
+                            {
+                              ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
+                              ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
+                              ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
+                              ESTADO <- gsub("Guan.*o.*", "Guanajuato", ESTADO)
+                              ESTADO <- gsub("M.*n.*", "Michoacan", ESTADO)
+                              ESTADO <- gsub("M.*los.*", "Morelos", ESTADO)
+                              ESTADO <- gsub("Nuevo Le.*n.*", "Nuevo Leon", ESTADO)
+                              ESTADO <- gsub("Quer.*ro.*", "Queretaro", ESTADO)
+                              ESTADO <- gsub("Zac.*cas.*", "Zacatecas", ESTADO)
+                              ESTADO <- gsub("Quint.*oo.*", "Quintana Roo", ESTADO)
+                              ESTADO <- gsub("San Luis Pot.*.*", "San Luis Potosi", ESTADO)
+                              ESTADO <- gsub("Yuca.*n.*", "Yucatan",ESTADO)
+                            }
+  )
+}
+
+for (i in 1:length(momujeres)) {
+  names(momujeres[[i]]) <- toupper(names(momujeres[[i]]))
+}
+
+for (j in 1:length(momujeres)) {
+  momujeres[[j]] <- within( momujeres[[j]] , 
+                            {
                               ESTADO <- gsub("M.*co.*", "Mexico", ESTADO)
                               ESTADO <- gsub("Agua.*es.*", "Aguascalientes", ESTADO)
                               ESTADO <- gsub("Dist.*al.*", "Distrito Federal", ESTADO)
