e2d09f9ebf087fb0b820bd727fd0705d4af60c46
Rayan <rayan.garciafa@gmail.com>
2016-10-14 16:30:58 -0500

    Se añaden códigos de pirámides

--- MordedurasPerroCodigo.R
+++ MordedurasPerroCodigo.R
@@ -414,12 +414,15 @@ tail(mpOrgData$MesDeOcurrencia)
 
 # ------------------------- Piramides ------------------------------
 
+# ------------------------- Piramides ------------------------------
+# Fuente de Notificacion hombres y mujeres
 # Hombres.
 hombres <- subset(mpOrgData$FuenteDeNotificacion, SEXO %in% "HOMBRES")
 mujeres <- subset(mpOrgData$FuenteDeNotificacion, SEXO %in% "MUJERES")
 estados <- c('Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato','Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit','Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi','Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan','Zacatecas')
 fuentes <- names(fng$`2004`)
 fuentes <- fuentes[-c(1,11)]
+
 dataframesPorEstado <- list()
 for (i in 1:length(estados)) {
   # Hombres
@@ -438,11 +441,140 @@ for (i in 1:length(estados)) {
     suma2 <- sum(subset(mujeresYEstadoYFuente$MORDEDURAS,! is.na(mujeresYEstadoYFuente$MORDEDURAS)))
     totales2 <- c(totales2, suma2)
   }
-  # Creacion de los data.drames.
+  # Creacion de los data.frames.
   dataframesPorEstado[[i]] <- data.frame(fuentes, totales1, totales2)
   names(dataframesPorEstado[[i]]) <- c("FUENTE", "HOMBRES", "MUJERES")
 }
+names(dataframesPorEstado) <- estados
 dataframesPorEstado
+
+
+#Pir??mide hombres y mujeres, por estado y fuente de notificacion  
+
+
+par(mfrow = c(4, 8), mar = c(0, 0, 1, 0), oma = c(0, 0, 1.2, 0))
+entidades <- c('Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato','Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit','Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi','Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan','Zacatecas')
+
+for (i in 1:32) {
+  PIR <- dataframesPorEstado[[i]]
+  Fem <- PIR[[3]]
+  Masc <- PIR[[2]]
+  
+  with(PIR, plot(0, 0, type = "n", xlim = c(-max(Masc), max(Fem)), ylim = c(0, 10), xlab ="",    ylab = "", axes = FALSE, bty="n", cex.main = 0.8, main = entidades[i]))
+  mtext(paste0("FUENTE DE NOTIFICACION", " (HOMBRES, MUJERES)"), side = 3, outer = TRUE)
+  
+  for(j in 1:10){
+    rect(-Masc[j],j, 0, j+1, col = "blue", border = "white")
+    rect(0,j, Fem[j], j+1, col = "red",  border = "white") 
+  }
+}
+
+
+
+# Grupo de Edad hombres y mujeres
+# Hombres.
+hombresg <- subset(mpOrgData$GrupoDeEdad, SEXO %in% "HOMBRES")
+mujeresg <- subset(mpOrgData$GrupoDeEdad, SEXO %in% "MUJERES")
+estados <- c('Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato','Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit','Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi','Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan','Zacatecas')
+grupos <- names(geg$`2004`)
+grupos <- toupper(grupos[-c(1,14)])
+
+dataframesPorEstadog <- list()
+for (i in 1:length(estados)) {
+  # Hombres
+  hombresYEstado <- subset(hombresg, ESTADO %in% estados[[i]])
+  totales1 <- c()  
+  for (j in 1:length(grupos)) {
+    hombresYEstadoYGrupo <- subset(hombresYEstado, EDAD %in% grupos[[j]])
+    suma <- sum(subset(hombresYEstadoYGrupo$MORDEDURAS,! is.na(hombresYEstadoYGrupo$MORDEDURAS)))
+    totales1 <- c(totales1, suma)
+  }
+  # Mujeres
+  mujeresYEstado <- subset(mujeresg, ESTADO %in% estados[[i]])
+  totales2 <- c()  
+  for (k in 1:length(grupos)) {
+    mujeresYEstadoYGrupo <- subset(mujeresYEstado, EDAD %in% grupos[[k]])
+    suma2 <- sum(subset(mujeresYEstadoYGrupo$MORDEDURAS,! is.na(mujeresYEstadoYGrupo$MORDEDURAS)))
+    totales2 <- c(totales2, suma2)
+  }
+  # Creacion de los data.frames.
+  dataframesPorEstadog[[i]] <- data.frame(grupos, totales1, totales2)
+  names(dataframesPorEstadog[[i]]) <- c("EDAD", "HOMBRES", "MUJERES")
+}
+names(dataframesPorEstadog) <- estados
+dataframesPorEstadog
+
+#Pir??mide hombres y mujeres, por estado y grupo de edad
+
+par(mfrow = c(4, 8), mar = c(0, 0, 1, 0), oma = c(0, 0, 1.2, 0))
+entidades <- c('Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato','Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit','Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi','Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan','Zacatecas')
+
+for (i in 1:32) {
+  PIRG <- dataframesPorEstadog[[i]]
+  Femg <- PIRG[[3]]
+  Mascg <- PIRG[[2]]
+  
+  with(PIRG, plot(0, 0, type = "n", xlim = c(-max(Mascg), max(Femg)), ylim = c(0, 12), xlab ="",    ylab = "", axes = FALSE, bty="n", cex.main = 0.8, main = entidades[i]))
+  mtext(paste0("GRUPO DE EDAD", " (HOMBRES, MUJERES)"), side = 3, outer = TRUE)
+  
+  for(j in 1:18){
+    rect(-Mascg[j],j, 0, j+1, col = "blue", border = "white")
+    rect(0,j, Femg[j], j+1, col = "red",  border = "white") 
+  }
+  
+}
+# Mes de ocurrencia hombres y mujeres
+# Hombres.
+hombresm <- subset(mpOrgData$MesDeOcurrencia, SEXO %in% "HOMBRES")
+mujeresm <- subset(mpOrgData$MesDeOcurrencia, SEXO %in% "MUJERES")
+estados <- c('Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato','Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit','Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi','Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan','Zacatecas')
+meses <- names(mog$`2004`)
+meses <- meses[-c(1)]
+
+dataframesPorEstadom <- list()
+for (i in 1:length(estados)) {
+  # Hombres
+  hombresYEstado <- subset(hombresm, ESTADO %in% estados[[i]])
+  totales1 <- c()  
+  for (j in 1:length(meses)) {
+    hombresYEstadoYMes <- subset(hombresYEstado, MES %in% meses[[j]])
+    suma <- sum(subset(hombresYEstadoYMes$MORDEDURAS,! is.na(hombresYEstadoYMes$MORDEDURAS)))
+    totales1 <- c(totales1, suma)
+  }
+  # Mujeres
+  mujeresYEstado <- subset(mujeresm, ESTADO %in% estados[[i]])
+  totales2 <- c()  
+  for (k in 1:length(meses)) {
+    mujeresYEstadoYMes <- subset(mujeresYEstado, MES %in% meses[[k]])
+    suma2 <- sum(subset(mujeresYEstadoYMes$MORDEDURAS,! is.na(mujeresYEstadoYMes$MORDEDURAS)))
+    totales2 <- c(totales2, suma2)
+  }
+  # Creacion de los data.frames.
+  dataframesPorEstadom[[i]] <- data.frame(meses, totales1, totales2)
+  names(dataframesPorEstadom[[i]]) <- c("MES", "HOMBRES", "MUJERES")
+}
+names(dataframesPorEstadom) <- estados
+dataframesPorEstadom
+
+#Pir??mide hombres y mujeres, mes de ocurrencia.
+
+par(mfrow = c(4, 8), mar = c(0, 0, 1, 0), oma = c(0, 0, 1.2, 0))
+entidades <- c('Aguascalientes', 'Baja California', 'Baja California Sur', 'Campeche', 'Chiapas', 'Chihuahua', 'Coahuila', 'Colima', 'Distrito Federal', 'Durango', 'Guanajuato','Guerrero', 'Hidalgo', 'Jalisco', 'Mexico', 'Michoacan', 'Morelos', 'Nayarit','Nuevo Leon', 'Oaxaca', 'Puebla', 'Queretaro', 'Quintana Roo', 'San Luis Potosi','Sinaloa', 'Sonora', 'Tabasco', 'Tamaulipas', 'Tlaxcala', 'Veracruz', 'Yucatan','Zacatecas')
+
+for (i in 1:32) {
+  PIRM <- dataframesPorEstadom[[i]]
+  Femm <- PIRM[[3]]
+  Mascm <- PIRM[[2]]
+  
+  with(PIRM, plot(0, 0, type = "n", xlim = c(-max(Mascm), max(Femm)), ylim = c(0, 14), xlab ="",    ylab = "", axes = FALSE, bty="n", cex.main = 0.8, main = entidades[i]))
+  mtext(paste0("MES DE OCURRENCIA", " (HOMBRES, MUJERES)"), side = 3, outer = TRUE)
+  
+  for(j in 1:12){
+    rect(-Mascm[j],j, 0, j+1, col = "blue", border = "white")
+    rect(0,j, Femm[j], j+1, col = "red",  border = "white") 
+  }
+}
+#Fin de pir??mides
 
 
 
